b1c69d2be244e569752c1762e3a075f285f431ff
Kerry Perret <kerry.perret@outlook.com>
2020-12-14 10:21:49 +0100

    feat(basics): add two impl for basic features

---
+++ src/lib/memory.go
@@ -0,0 +1,22 @@
+package lib
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+func printMemoryUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Println("\n---")
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+	fmt.Println("---")
+}
